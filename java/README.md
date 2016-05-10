73648135a3fe2cbd0c53c48ce5d3335fb608ef3a
Kevin Cooper <cooperka92@gmail.com>
2016-05-09 22:18:56 -0700

    Init Java directory (using Maven)

---
+++ java/README.md
@@ -0,0 +1,7 @@
+# Babel Deathmatch - Java
+
+## Usage
+
+```shell
+mvn exec:java -Dexec.mainClass="com.cooperka.App"
+```
---
+++ java/src/test/java/com/cooperka/AppTest.java
@@ -0,0 +1,20 @@
+package com.cooperka;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Mock test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+}
